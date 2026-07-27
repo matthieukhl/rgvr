b8413ab774435259d3fabe6117bd63519dbebda6
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-07-27 23:31:11 +0200

    added logout command

--- cmd/logout.go
+++ cmd/logout.go
@@ -1,13 +1,29 @@
 /*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+		rgvr - A CLI to interact with Ringover's public API.
+	    Copyright (C) 2026  Matthieu Khairallah
 
+	    This program is free software: you can redistribute it and/or modify
+	    it under the terms of the GNU Affero General Public License as published by
+	    the Free Software Foundation, either version 3 of the License, or
+	    (at your option) any later version.
+
+	    This program is distributed in the hope that it will be useful,
+	    but WITHOUT ANY WARRANTY; without even the implied warranty of
+	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	    GNU Affero General Public License for more details.
+
+	    You should have received a copy of the GNU Affero General Public License
+	    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // logoutCmd represents the logout command
@@ -20,21 +36,22 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("logout called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		configFile := viper.ConfigFileUsed()
+
+		if configFile == "" {
+			fmt.Println("Not currently logged in.")
+			return nil
+		}
+
+		if err := os.Remove(configFile); err != nil {
+			return fmt.Errorf("removing config file: %w", err)
+		}
+		fmt.Println("Successfully logged out.")
+		return nil
 	},
 }
 
 func init() {
 	authCmd.AddCommand(logoutCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// logoutCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// logoutCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
