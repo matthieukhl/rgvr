24661cb592dda8506f6d441c8c625f45f23bf698
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-07-28 00:17:16 +0200

    feat: added teams command that retrieves and displays team information

---
+++ cmd/teams.go
@@ -0,0 +1,85 @@
+/*
+		rgvr - A CLI to interact with Ringover's public API.
+	    Copyright (C) 2026  Matthieu Khairallah
+
+	    This program is free software: you can redistribute it and/or modify
+	    it under the terms of the GNU Affero General Public License as published by
+	    the Free Software Foundation, either version 3 of the License, or
+	    (at your option) any later version.
+
+	    This program is distributed in the hope that it will be useful,
+	    but WITHOUT ANY WARRANTY; without even the implied warranty of
+	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	    GNU Affero General Public License for more details.
+
+	    You should have received a copy of the GNU Affero General Public License
+	    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/matthieukhl/rgvr/internal"
+	"github.com/matthieukhl/rgvr/internal/models"
+	"github.com/spf13/cobra"
+)
+
+// teamsCmd represents the teams command
+var teamsCmd = &cobra.Command{
+	Use:   "teams",
+	Short: "Retrieve team information.",
+	Long:  `Retrieves a complete team object containing lists of numbers, users, ivrs, conferences, tags and groups.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		client, err := internal.NewClient()
+		if err != nil {
+			return fmt.Errorf("creating client: %w", err)
+		}
+
+		ctx := context.WithValue(cmd.Context(), clientContextKey, client)
+		cmd.SetContext(ctx)
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path := "/teams"
+		client := cmd.Context().Value(clientContextKey).(*internal.Client)
+
+		resp, err := client.Get(path)
+		if err != nil {
+			return fmt.Errorf("retrieving team information: %w", err)
+		}
+		defer resp.Body.Close()
+
+		var team models.Team
+		if err = json.NewDecoder(resp.Body).Decode(&team); err != nil {
+			return fmt.Errorf("decoding team information: %w", err)
+		}
+
+		fmt.Printf("Team ID: %d\n", team.TeamID)
+		fmt.Printf("Name: %s\n", team.Name)
+		fmt.Printf("Total Numbers Count: %d\n", team.TotalNumbersCount)
+		fmt.Printf("Total Users Count: %d\n", team.TotalUsersCount)
+		fmt.Printf("Total Conferences Count: %d\n", team.TotalConferencesCount)
+		fmt.Printf("Total Tags Count: %d\n", team.TotalTagsCount)
+		fmt.Printf("Total Groups Count: %d\n", team.TotalGroupsCount)
+
+		return nil
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(teamsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// teamsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// teamsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ internal/models/team.go
@@ -0,0 +1,150 @@
+package models
+
+import "time"
+
+type Team struct {
+	TeamID            int    `json:"team_id"`
+	Name              string `json:"name"`
+	TotalNumbersCount int    `json:"total_numbers_count"`
+	Numbers           []struct {
+		Number       int64  `json:"number"`
+		Label        string `json:"label"`
+		Type         string `json:"type"`
+		UserID       int    `json:"user_id"`
+		IvrID        int    `json:"ivr_id"`
+		ConferenceID int    `json:"conference_id"`
+		IsSms        bool   `json:"is_sms"`
+		IsSmsWrite   bool   `json:"is_sms_write"`
+		IsCallable   bool   `json:"is_callable"`
+		Format       struct {
+			Raw              int    `json:"raw"`
+			CountryCode      string `json:"country_code"`
+			Country          string `json:"country"`
+			E164             string `json:"e164"`
+			International    string `json:"international"`
+			InternationalAlt string `json:"international_alt"`
+			National         string `json:"national"`
+			NationalAlt      string `json:"national_alt"`
+			Rfc3966          string `json:"rfc3966"`
+			IsShortCode      bool   `json:"is_short_code"`
+		} `json:"format"`
+	} `json:"numbers"`
+	TotalUsersCount int `json:"total_users_count"`
+	Users           []struct {
+		UserID     int    `json:"user_id"`
+		TeamID     int    `json:"team_id"`
+		Initial    string `json:"initial"`
+		Color      string `json:"color"`
+		Firstname  string `json:"firstname"`
+		Lastname   string `json:"lastname"`
+		Company    string `json:"company"`
+		Email      string `json:"email"`
+		Picture    string `json:"picture"`
+		ConcatName string `json:"concat_name"`
+		Numbers    []struct {
+			Number       int64  `json:"number"`
+			Label        string `json:"label"`
+			Type         string `json:"type"`
+			UserID       int    `json:"user_id"`
+			IvrID        int    `json:"ivr_id"`
+			ConferenceID int    `json:"conference_id"`
+			IsSms        bool   `json:"is_sms"`
+			IsSmsWrite   bool   `json:"is_sms_write"`
+			IsCallable   bool   `json:"is_callable"`
+			Format       struct {
+				Raw              int    `json:"raw"`
+				CountryCode      string `json:"country_code"`
+				Country          string `json:"country"`
+				E164             string `json:"e164"`
+				International    string `json:"international"`
+				InternationalAlt string `json:"international_alt"`
+				National         string `json:"national"`
+				NationalAlt      string `json:"national_alt"`
+				Rfc3966          string `json:"rfc3966"`
+				IsShortCode      bool   `json:"is_short_code"`
+			} `json:"format"`
+		} `json:"numbers"`
+	} `json:"users"`
+	TotalIvrsCount int `json:"total_ivrs_count"`
+	Ivrs           []struct {
+		IvrID   int    `json:"ivr_id"`
+		Name    string `json:"name"`
+		Color   string `json:"color"`
+		Numbers []struct {
+			Number       int64  `json:"number"`
+			Label        string `json:"label"`
+			Type         string `json:"type"`
+			UserID       int    `json:"user_id"`
+			IvrID        int    `json:"ivr_id"`
+			ConferenceID int    `json:"conference_id"`
+			IsSms        bool   `json:"is_sms"`
+			IsSmsWrite   bool   `json:"is_sms_write"`
+			IsCallable   bool   `json:"is_callable"`
+			Format       struct {
+				Raw              int    `json:"raw"`
+				CountryCode      string `json:"country_code"`
+				Country          string `json:"country"`
+				E164             string `json:"e164"`
+				International    string `json:"international"`
+				InternationalAlt string `json:"international_alt"`
+				National         string `json:"national"`
+				NationalAlt      string `json:"national_alt"`
+				Rfc3966          string `json:"rfc3966"`
+				IsShortCode      bool   `json:"is_short_code"`
+			} `json:"format"`
+		} `json:"numbers"`
+		Scenarios []struct {
+			ScenarioID   int    `json:"scenario_id"`
+			IvrID        int    `json:"ivr_id"`
+			Name         string `json:"name"`
+			Color        string `json:"color"`
+			ScenarioType string `json:"scenario_type"`
+			IsDefault    bool   `json:"is_default"`
+		} `json:"scenarios"`
+		IsOpen bool `json:"is_open"`
+	} `json:"ivrs"`
+	TotalConferencesCount int `json:"total_conferences_count"`
+	Conferences           []struct {
+		ConferenceID int    `json:"conference_id"`
+		Name         string `json:"name"`
+		Numbers      []struct {
+			Number       int64  `json:"number"`
+			Label        string `json:"label"`
+			Type         string `json:"type"`
+			UserID       int    `json:"user_id"`
+			IvrID        int    `json:"ivr_id"`
+			ConferenceID int    `json:"conference_id"`
+			IsSms        bool   `json:"is_sms"`
+			IsSmsWrite   bool   `json:"is_sms_write"`
+			IsCallable   bool   `json:"is_callable"`
+			Format       struct {
+				Raw              int    `json:"raw"`
+				CountryCode      string `json:"country_code"`
+				Country          string `json:"country"`
+				E164             string `json:"e164"`
+				International    string `json:"international"`
+				InternationalAlt string `json:"international_alt"`
+				National         string `json:"national"`
+				NationalAlt      string `json:"national_alt"`
+				Rfc3966          string `json:"rfc3966"`
+				IsShortCode      bool   `json:"is_short_code"`
+			} `json:"format"`
+		} `json:"numbers"`
+	} `json:"conferences"`
+	TotalTagsCount int `json:"total_tags_count"`
+	Tags           []struct {
+		TagID        int       `json:"tag_id"`
+		Name         string    `json:"name"`
+		Color        string    `json:"color"`
+		Description  string    `json:"description"`
+		CreationDate time.Time `json:"creation_date"`
+	} `json:"tags"`
+	TotalGroupsCount int `json:"total_groups_count"`
+	Groups           []struct {
+		GroupID         int    `json:"group_id"`
+		Name            string `json:"name"`
+		TotalUsersCount int    `json:"total_users_count"`
+		Color           string `json:"color"`
+		IsJumper        bool   `json:"is_jumper"`
+	} `json:"groups"`
+}
