e84941cbe3e97b21f7e96fa45b8050bb2a77b86b
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-07-28 22:56:18 +0200

    chore: refactored team struct

--- internal/models/team.go
+++ internal/models/team.go
@@ -20,97 +20,20 @@ package models
 
 import "time"
 
+// Team represents a team in the Ringover system, including its associated numbers, users, IVRs, conferences, tags, and groups.
 type Team struct {
-	TeamID            int    `json:"team_id"`
-	Name              string `json:"name"`
-	TotalNumbersCount int    `json:"total_numbers_count"`
-	Numbers           []struct {
-		Number       int64  `json:"number"`
-		Label        string `json:"label"`
-		Type         string `json:"type"`
-		UserID       int    `json:"user_id"`
-		IvrID        int    `json:"ivr_id"`
-		ConferenceID int    `json:"conference_id"`
-		IsSms        bool   `json:"is_sms"`
-		IsSmsWrite   bool   `json:"is_sms_write"`
-		IsCallable   bool   `json:"is_callable"`
-		Format       struct {
-			Raw              int    `json:"raw"`
-			CountryCode      string `json:"country_code"`
-			Country          string `json:"country"`
-			E164             string `json:"e164"`
-			International    string `json:"international"`
-			InternationalAlt string `json:"international_alt"`
-			National         string `json:"national"`
-			NationalAlt      string `json:"national_alt"`
-			Rfc3966          string `json:"rfc3966"`
-			IsShortCode      bool   `json:"is_short_code"`
-		} `json:"format"`
-	} `json:"numbers"`
-	TotalUsersCount int `json:"total_users_count"`
-	Users           []struct {
-		UserID     int    `json:"user_id"`
-		TeamID     int    `json:"team_id"`
-		Initial    string `json:"initial"`
-		Color      string `json:"color"`
-		Firstname  string `json:"firstname"`
-		Lastname   string `json:"lastname"`
-		Company    string `json:"company"`
-		Email      string `json:"email"`
-		Picture    string `json:"picture"`
-		ConcatName string `json:"concat_name"`
-		Numbers    []struct {
-			Number       int64  `json:"number"`
-			Label        string `json:"label"`
-			Type         string `json:"type"`
-			UserID       int    `json:"user_id"`
-			IvrID        int    `json:"ivr_id"`
-			ConferenceID int    `json:"conference_id"`
-			IsSms        bool   `json:"is_sms"`
-			IsSmsWrite   bool   `json:"is_sms_write"`
-			IsCallable   bool   `json:"is_callable"`
-			Format       struct {
-				Raw              int    `json:"raw"`
-				CountryCode      string `json:"country_code"`
-				Country          string `json:"country"`
-				E164             string `json:"e164"`
-				International    string `json:"international"`
-				InternationalAlt string `json:"international_alt"`
-				National         string `json:"national"`
-				NationalAlt      string `json:"national_alt"`
-				Rfc3966          string `json:"rfc3966"`
-				IsShortCode      bool   `json:"is_short_code"`
-			} `json:"format"`
-		} `json:"numbers"`
-	} `json:"users"`
-	TotalIvrsCount int `json:"total_ivrs_count"`
-	Ivrs           []struct {
-		IvrID   int    `json:"ivr_id"`
-		Name    string `json:"name"`
-		Color   string `json:"color"`
-		Numbers []struct {
-			Number       int64  `json:"number"`
-			Label        string `json:"label"`
-			Type         string `json:"type"`
-			UserID       int    `json:"user_id"`
-			IvrID        int    `json:"ivr_id"`
-			ConferenceID int    `json:"conference_id"`
-			IsSms        bool   `json:"is_sms"`
-			IsSmsWrite   bool   `json:"is_sms_write"`
-			IsCallable   bool   `json:"is_callable"`
-			Format       struct {
-				Raw              int    `json:"raw"`
-				CountryCode      string `json:"country_code"`
-				Country          string `json:"country"`
-				E164             string `json:"e164"`
-				International    string `json:"international"`
-				InternationalAlt string `json:"international_alt"`
-				National         string `json:"national"`
-				NationalAlt      string `json:"national_alt"`
-				Rfc3966          string `json:"rfc3966"`
-				IsShortCode      bool   `json:"is_short_code"`
-			} `json:"format"`
-		} `json:"numbers"`
+	TeamID            int      `json:"team_id"`
+	Name              string   `json:"name"`
+	TotalNumbersCount int      `json:"total_numbers_count"`
+	Numbers           []Number `json:"numbers"`
+	TotalUsersCount   int      `json:"total_users_count"`
+	Users             []User   `json:"users"`
+	TotalIvrsCount    int      `json:"total_ivrs_count"`
+	Ivrs              []struct {
+		IvrID     int      `json:"ivr_id"`
+		Name      string   `json:"name"`
+		Color     string   `json:"color"`
+		Numbers   []Number `json:"numbers"`
 		Scenarios []struct {
 			ScenarioID   int    `json:"scenario_id"`
 			IvrID        int    `json:"ivr_id"`
@@ -123,31 +46,9 @@ type Team struct {
 	} `json:"ivrs"`
 	TotalConferencesCount int `json:"total_conferences_count"`
 	Conferences           []struct {
-		ConferenceID int    `json:"conference_id"`
-		Name         string `json:"name"`
-		Numbers      []struct {
-			Number       int64  `json:"number"`
-			Label        string `json:"label"`
-			Type         string `json:"type"`
-			UserID       int    `json:"user_id"`
-			IvrID        int    `json:"ivr_id"`
-			ConferenceID int    `json:"conference_id"`
-			IsSms        bool   `json:"is_sms"`
-			IsSmsWrite   bool   `json:"is_sms_write"`
-			IsCallable   bool   `json:"is_callable"`
-			Format       struct {
-				Raw              int    `json:"raw"`
-				CountryCode      string `json:"country_code"`
-				Country          string `json:"country"`
-				E164             string `json:"e164"`
-				International    string `json:"international"`
-				InternationalAlt string `json:"international_alt"`
-				National         string `json:"national"`
-				NationalAlt      string `json:"national_alt"`
-				Rfc3966          string `json:"rfc3966"`
-				IsShortCode      bool   `json:"is_short_code"`
-			} `json:"format"`
-		} `json:"numbers"`
+		ConferenceID int      `json:"conference_id"`
+		Name         string   `json:"name"`
+		Numbers      []Number `json:"numbers"`
 	} `json:"conferences"`
 	TotalTagsCount int `json:"total_tags_count"`
 	Tags           []struct {
