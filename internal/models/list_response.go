378a146fd8dac67312d7a6ee9ac29b8e93e470f4
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-07-28 22:55:14 +0200

    feat: added generic struct that matches ringover's api payload structure: list_count + list

---
+++ internal/models/list_response.go
@@ -0,0 +1,27 @@
+/*
+		rgvr - A CLI to interact with Ringover's public API.
+	    Copyright (C) 2026  Matthieu Khairallah
+
+	    This program is free software: you can redistribute it and/or modify
+	    it under the terms of the GNU Affero General Public License as published by
+	    the Free Software Foundation, either version 3 of the License, or
+	    (at your option) any later version.
+
+	    This program is distributed in the hope that it will be useful,
+	    but WITHOUT ANY WARRANTY; without even the implied warranty of
+	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	    GNU Affero General Public License for more details.
+
+	    You should have received a copy of the GNU Affero General Public License
+	    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package models
+
+// Ringover's API returns a list of items in a paginated format.
+// The ListResponse struct is a generic representation of such a response,
+// where T can be any type that represents the items in the list.
+type ListResponse[T any] struct {
+	ListCount int `json:"list_count"`
+	List      []T `json:"list"`
+}
