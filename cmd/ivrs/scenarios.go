f0fc98541224ccee43b30860f18653b382f74d3a
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2026-08-09 23:31:07 +0200

    feat: added 'ivrs scenarios get <ivr_id> <scenario_id> command

--- cmd/ivrs/scenarios.go
+++ cmd/ivrs/scenarios.go
@@ -11,4 +11,5 @@ var scenariosCmd = &cobra.Command{
 
 func init() {
 	ivrsCmd.AddCommand(scenariosCmd)
+	scenariosCmd.PersistentFlags().String("format", "json", "Choose the output's format: table / json")
 }
